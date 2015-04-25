b60dd093f036ef1053cb62066051b352065985f8
Maxime Bittan <maxime.bittan@gmail.com>
2015-04-25 13:05:14 +0200

    Add speedup script

---
+++ src/scripts/speedup.sh
@@ -0,0 +1,169 @@
+#! /bin/bash
+
+#Path leading to the directory containing the xml files (translations.xml and
+#profiles.xml)
+XMLDIR=/home/maxime/Bureau/psar/src/routino-2.7.3/src/xml
+
+#Directory that contains the *.mem files
+MEMDIR=/home/maxime/Bureau/psar/src/routino-2.7.3/src/
+
+#File containing the list of waypoints you want to use.
+WAYPOINTSFILE="";
+
+#Path of the non multithreaded router
+ROUTERPATH=/home/maxime/Bureau/psar/src/routino-2.7.3/src/old/router
+
+#Path of the multithreaded router
+MTROUTERPATH=/home/maxime/Bureau/psar/src/routino-2.7.3/src/router
+
+#Type of transport to use
+TRANSPORT="bicycle";
+
+#Output file
+OUTPUTFILE="values.out";
+
+#Number of thread for the multithreaded router
+NBTHREADS="3";
+
+print_usage () {
+    cat <<EOF
+Usage : $0 [Options]
+
+Options :
+  --xmldir=<dir>        Path to the directory containing the xml files.
+                          Default : $XMLDIR
+  --memdir=<dir>        Path leading to the *.mem files.
+                          Default : $MEMDIR
+  --waypoints=<file>    Path of the file containing the waypoints. 
+                          Mandatory.
+  --router=<file>       Path to the non-multithreaded router
+                          Default : $ROUTERPATH
+  --mtrouter=<file>     Path to the multithreaded router
+                          Default : $MTROUTERPATH
+  --transport=<type>    Type of transport to use. Can be foot, horse,
+                          wheelchair, bicycle, moped, motorcycle, motorcar.
+                          Default : $TRANSPORT
+  --output=<file>       Output file.
+                          Default : $OUTPUTFILE
+  --threads=<number>    Number of threads to use.
+                          Default : $NBTHREADS
+EOF
+}
+
+while [ ! -z "$1" ]
+do
+    case $1 in 
+	--xmldir=*)
+	    XMLDIR=${1#--*=};
+	    [ -d "$XMLDIR" ] || {
+		echo "ERR : $XMLDIR is not a reachable directory";
+		exit 1;
+	    }
+	    shift 1;;
+	--memdir=*)
+	    MEMDIR=${1#--*=};
+	    [ -d "$MEMDIR" ] || {
+		echo "ERR : $MEMDIR is not a reachable directory";
+		exit 1;
+	    }
+	    shift 1;;
+	--waypoints=*)
+	    WAYPOINTSFILE=${1#--*=};
+	    [ -f "$WAYPOINTSFILE" ] || {
+		echo "ERR : $WAYPOINTSFILE does not exist"
+		exit 1;
+	    }
+	    shift 1;;
+	--router=*)
+	    ROUTERPATH=${1#--*=};
+	    [ -x "$ROUTERPATH" ] || {
+		echo "ERR : $ROUTERPATH is not an executable file";
+		exit 1;
+	    }
+	    shift 1;;
+	--mtrouter=*)
+	    MTROUTERPATH=${1#--*=};
+	    [ -x "$MTROUTERPATH" ] || {
+		echo "ERR : $MTROUTERPATH is not an executable file";
+		exit 1;
+	    }
+	    shift 1;;
+	--transport=*)
+	    TRANSPORT=${1#--*=};
+	    case "$TRANSPORT" in
+		foot|horse|wheelchair|bicycle|moped|motorcycle|motorcar)
+		    ;;
+		*)
+		    echo "ERR : $TRANSPORT is not a valid type of transport"
+		    exit 1;;
+	    esac
+	    shift 1;;
+	--output=*)
+	    OUTPUTFILE=${1#--*=};
+	    shift 1;;
+	--threads=*)
+	    NBTHREADS=${1#--*=};
+	    [[ "$NBTHREADS" =~ ^[0-9]+$ ]] || {
+		echo "ERR : $NBTHRADS is not a number";
+		exit 1;
+	    }
+	    shift 1;;
+	--help|-h)
+	    print_usage;
+	    exit 1;;
+	--*)
+	    echo "ERR : unknown option $1"
+	    exit 1;;
+	*)
+	    echo "Invalid number of arguments"
+	    exit 1;;
+    esac;
+done;
+
+
+[ -z "$WAYPOINTSFILE" ] && {
+    echo "ERR : No waypoints file specified. --waypoints is mandatory."
+    exit 1;
+}
+
+CMDLINE="$ROUTERPATH --transport=$TRANSPORT --dir=$MEMDIR --profiles=$XMLDIR/profiles.xml --translations=$XMLDIR/translations.xml "
+MTCMDLINE="$MTROUTERPATH --threads=$NBTHREADS --transport=$TRANSPORT --dir=$MEMDIR --profiles=$XMLDIR/profiles.xml --translations=$XMLDIR/translations.xml "
+
+{
+    read lat lon 
+    CMDLINE="$CMDLINE --lat1=$lat --lon1=$lon"
+    MTCMDLINE="$MTCMDLINE --lat1=$lat --lon1=$lon"
+    read lat lon 
+    CMDLINE="$CMDLINE --lat2=$lat --lon2=$lon"
+    MTCMDLINE="$MTCMDLINE --lat2=$lat --lon2=$lon"
+    
+    index=2;
+    total=$(wc -l $WAYPOINTSFILE | cut -f 1 -d " ");
+    
+    env echo -n "" > $OUTPUTFILE;
+    
+    while [[ ! -z "$lon" && ! -z "$lat" ]]
+    do
+	start=$(date +%s%N);
+	$CMDLINE >/dev/null
+	end=$(date +%s%N);
+	routertime=$(((end-start)/1000000));
+
+	start=$(date +%s%N);
+	$MTCMDLINE >/dev/null
+	end=$(date +%s%N);
+	mtroutertime=$(((end-start)/1000000));
+	
+	echo "$index $routertime $mtroutertime">>$OUTPUTFILE;
+#	env echo -e "\n\n$MTCMDLINE\n\n"
+#	env echo -e "\n\n$CMDLINE\n\n
+	env echo -e -n "\b\b\b\b$(((index*100)/total))%";
+
+	index=$((index+1))
+	read lat lon 
+	CMDLINE="$CMDLINE --lat$index=$lat --lon$index=$lon"
+	MTCMDLINE="$MTCMDLINE --lat$index=$lat --lon$index=$lon"
+    done;
+
+    echo "";
+}< $WAYPOINTSFILE
